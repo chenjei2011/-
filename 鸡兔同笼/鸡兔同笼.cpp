8d6340f24001a02bd5c573a8813155787663f220
chenjei2011 <2963169251@qq.com>
2025-01-12 11:20:16 +0800

    Add files via upload

---
+++ 鸡兔同笼/鸡兔同笼.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>//头文件
+
+int main() {
+    int heads, legs;
+    int chickens, rabbits;
+
+    // 假设总头数总腿数
+    heads = 100;
+    legs = 350;
+
+    // 解方程
+    rabbits = legs / 2 - heads;
+    
+    chickens = heads - rabbits;
+
+    // 检查
+    if (2 * chickens + 4 * rabbits == legs) {
+        std::cout << "鸡的数量为: " << chickens << std::endl;
+        std::cout << "兔的数量为: " << rabbits << std::endl;
+    }
+    else {
+        std::cout << "没有合理的解。" << std::endl;
+    }
+
+    return 0;
+}
+
+
---
+++ 鸡兔同笼2.0/鸡兔同笼2.0.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+int main() {
+    int heads, legs;//腿数与头数
+    int chickens, rabbits;//鸡与兔
+
+    // 提示用户输入头数和腿数
+    std::cout << "请输入头数: ";
+    std::cin >> heads;
+    std::cout << "请输入腿数: ";
+    std::cin >> legs;
+    if (legs % 2 != 0 || heads <= 0 || legs < 2 * heads || legs > 4 * heads) {
+        std::cout << "无解" << std::endl;//判断是否符合条件
+    }
+    else {
+        chickens = 2 * heads - legs / 2;//计算
+        rabbits = heads - chickens;
+
+        std::cout << "鸡的数量: " << chickens << std::endl;
+        std::cout << "兔的数量: " << rabbits << std::endl;//输出结果
+    }
+
+    return 0;
+}
